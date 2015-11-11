e6156d4819916bb847a830435898006d6e28bcb3
Alejandro Seguí <alesegdia@gmail.com>
2015-11-11 22:11:16 +0100

    removing unused tmp userdata var, was the same as currentRectSize; using new function to access frame user data

--- app/tools/boxedit.js
+++ app/tools/boxedit.js
@@ -9,10 +9,10 @@ var BoxTool = (function() {
 	box.prototype.constructor = box;
 
 	box.prototype.createFrameUserData = function() {
+		// for temporal rect drawing until mouseUp
 		return {
-			currentRectTL : [],
-			currentRectSize : [],
-			tmp : [ 0, 0 ],
+			currentRectTL 	: null,
+			currentRectSize : null,
 		};
 	};
 
@@ -21,43 +21,43 @@ var BoxTool = (function() {
 	};
 
 	box.prototype.mouseDownListener = function(x,y) {
-		this.currentFrameData.userdata.currentRectTL = [ x, y ];
-		this.currentFrameData.userdata.tmp = [ 0, 0 ];
+		this.getFrameUserData().currentRectTL = [ x, y ];
+		this.getFrameUserData().currentRectSize = [ 0, 0 ];
 	};
 
 	box.prototype.mouseUpListener = function(x,y) {
 		var min = [
-			Math.min(this.currentFrameData.userdata.currentRectTL[0], x),
-			Math.min(this.currentFrameData.userdata.currentRectTL[1], y) ];
+			Math.min(this.getFrameUserData().currentRectTL[0], x),
+			Math.min(this.getFrameUserData().currentRectTL[1], y) ];
 		var max = [
-			Math.max(this.currentFrameData.userdata.currentRectTL[0], x),
-			Math.max(this.currentFrameData.userdata.currentRectTL[1], y) ];
-		this.currentFrameData.currentRectSize = [max[0] - min[0], max[1] - min[1]];
-		this.insertNewElement({ pos: min, size: this.currentFrameData.currentRectSize });
-		this.currentFrameData.userdata.currentRectTL = null;
-		this.currentFrameData.userdata.tmp = null;
+			Math.max(this.getFrameUserData().currentRectTL[0], x),
+			Math.max(this.getFrameUserData().currentRectTL[1], y) ];
+		this.getFrameUserData().currentRectSize = [max[0] - min[0], max[1] - min[1]];
+		this.insertNewElement({ pos: min, size: this.getFrameUserData().currentRectSize });
+		this.getFrameUserData().currentRectTL = null;
+		this.getFrameUserData().currentRectSize = null;
 	};
 
 	box.prototype.mouseMoveListener = function(x,y) {
-		if( this.currentFrameData.userdata.tmp != null )
+		if( this.getFrameUserData().currentRectSize != null )
 		{
-			this.currentFrameData.userdata.tmp[0] = x - this.currentFrameData.userdata.currentRectTL[0];
-			this.currentFrameData.userdata.tmp[1] = y - this.currentFrameData.userdata.currentRectTL[1];
+			this.getFrameUserData().currentRectSize[0] = x - this.getFrameUserData().currentRectTL[0];
+			this.getFrameUserData().currentRectSize[1] = y - this.getFrameUserData().currentRectTL[1];
 		}
 	};
 
 	box.prototype.render = function(x,y) {
-		for( var i = 0; i < this.currentFrameData.elements.length; i++ )
+		for( var i = 0; i < this.getFrameElements().length; i++ )
 		{
-			var r = this.currentFrameData.elements[i].data;
+			var r = this.getFrameElements()[i].data;
 			ctx.strokeStyle = "rgba(0,0,255,255)";
 			ctx.strokeRect(r.pos[0], r.pos[1], r.size[0], r.size[1]);
 		}
 
-		if( this.currentFrameData.userdata.currentRectTL != null && this.currentFrameData.userdata.tmp != null )
+		if( this.getFrameUserData().currentRectTL != null && this.getFrameUserData().currentRectSize != null )
 		{
-			var pos = this.currentFrameData.userdata.currentRectTL;
-			var size = this.currentFrameData.userdata.tmp;
+			var pos = this.getFrameUserData().currentRectTL;
+			var size = this.getFrameUserData().currentRectSize;
 			ctx.strokeStyle = "rgba(255,0,0,255)";
 			ctx.strokeRect(pos[0], pos[1], size[0], size[1]);
 		}
